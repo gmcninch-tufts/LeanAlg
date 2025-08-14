9794ec6610d0e3fd42fa37b31ae7f1c68107f154
George McNinch <gmcninch@gmail.com>
2025-08-13 22:10:50 -0400

    symm<->mvpoly work

--- LeanAlg/CnFields/Proposal.lean
+++ LeanAlg/CnFields/Proposal.lean
@@ -2,7 +2,13 @@ import Mathlib
 
 universe μ ν 
 
-variable (k : Type μ) [Field k]
+variable (ι : Type)
+variable (k : Type) [Field k]
+variable (V : Type) [AddCommGroup V] [Module k V]
+
+#check (ι : Type) × (MvPolynomial ι k) × (Module.Basis ι k V)
+
+#check Σ (ι : Type), (MvPolynomial ι k) × (Module.Basis ι k V)
 
 def IsCr' (r : {q :ℚ // q≥0 }) : Prop :=
   ∀ (n : ℕ),
@@ -11,3 +17,51 @@ def IsCr' (r : {q :ℚ // q≥0 }) : Prop :=
   MvPolynomial.IsHomogeneous f d → (d:ℝ) ^ (r:ℝ) < (n:ℝ) → 
   ∃ v  : Fin n → k, f.eval v = 0 ∧ v ≠ 0
 
+
+--def RegularFunction (k : Type*) [Field k] (V : Type*) [AddCommGroup V] [Module k V]
+  
+def PreRegularFunction (k : Type) [Field k] (V : Type) [AddCommGroup V] [Module k V]
+  :  Type 1 :=
+  Σ (ι : Type), (MvPolynomial ι k) × (Module.Basis ι k V)
+  
+def MvPolynomial.LinearChangeOfVar (k : Type) [Field k] (σ τ : Type) (φ : (σ →₀ k) ≃ₗ[k] τ →₀ k) :
+  MvPolynomial σ k ≃ₐ[k] MvPolynomial τ k where
+    toFun := _
+    invFun := _
+    left_inv := _
+    right_inv := _
+    map_mul' := _
+    map_add' := _
+    commutes' := _
+  
+inductive RegularFunction (k : Type) [Field k] (V : Type) [AddCommGroup V] [Module k V]
+  
+instance [AddCommGroup V] [Module k V] : Module k (RegularFunction k V) := by sorry
+  
+def RegularFunctionAssocPolynomial (k : Type) [Field k] (V : Type) [AddCommGroup V] [Module k V]
+  [FiniteDimensional k V] (ι : Type*) (b : Module.Basis ι k V)
+  : RegularFunction k V ≃ MvPolynomial ι k := by sorry
+
+def EvalRegularFunction (k : Type) [Field k] (V : Type) [AddCommGroup V] [Module k V]
+  (f : RegularFunction k V) (v : V) : k := by sorry
+
+def 
+
+/- It would arguably be nice to replace `MvPolynomial` in the
+  definition above with a coordinate-free version of homogeneous form.
+  Arguably something like the following:
+  
+  - a definition of `RegularFunction` depending on `(k : Type*) (V :
+    Type*) [Field k] [AddCommGroup V] [Module k V]`
+	
+	Types as follows:
+	
+	```
+	def RegularFunctionAssocPolynomial (k : Type*) [Field k] (V : Type*) [AddCommGroup V] [Module k V] [FiniteDimensional k V]
+	  (f : RegularFunction k V) (ι : Type*) (b : Module.Basis ι k V)
+      : MvPolynomial ι k := by sorry
+
+	def EvalRegularFunction (k : Type*) [Field k] (V : Type*) [AddCommGroup V] [Module k V] 
+      (f : RegularFunction k V) (v : V) : k := by sorry
+	```
+-/
---
+++ LeanAlg/RegularFunctions/mvp.lean
@@ -0,0 +1,103 @@
+import Mathlib
+
+noncomputable section
+
+universe μ ν ω
+
+variable {R : Type μ} {M : Type ν} [CommRing R] [AddCommGroup M] [Module R M]
+
+open BigOperators in
+example (φ : M →ₗ[R] M) (I : Type) (f : I →₀ M) :
+  φ ( ∑ i ∈ f.support, f i ) = ∑ i∈ f.support, φ (f i) := by
+    simp_all only [map_sum]
+
+open BigOperators in
+example (I : Type) (f : I →₀ M) (p : M → Prop) (hadd : ∀ x y : M, p x → p y → p (x + y))
+  (h0 : p 0) (hI : (i : I) → p (f i)) :
+  p (∑ i ∈ f.support, f i) := by
+    apply Finset.sum_induction f p hadd h0
+    intro i _
+    exact hI i
+
+
+theorem SymmetricAlgebra.induction_basis [Module.Free R M] (σ : Type ω) [Fintype σ]
+    (b : Module.Basis σ R M)
+    {motive : SymmetricAlgebra R M → Prop}
+    (algebraMap : ∀ r, motive (algebraMap R (SymmetricAlgebra R M) r))
+    (basis : ∀ i, motive (ι R M (b i)))
+    (unit : motive 0)
+    (mul : ∀ a b, motive a → motive b → motive (a * b))
+    (add : ∀ a b, motive a → motive b → motive (a + b))
+    (a : SymmetricAlgebra R M) : motive a :=
+  have : ∀ x : M, motive (ι R M x) := fun x => by
+    rw [ ← Module.Basis.sum_repr b x ]
+    rw [ map_sum ]
+    apply Finset.sum_induction
+    case hom => exact add
+    case unit => exact unit
+    case base =>
+      intro i _
+      rw [ map_smul, Algebra.smul_def ]
+      apply mul
+      · apply algebraMap
+      · apply basis
+  SymmetricAlgebra.induction R M (motive := motive) algebraMap this mul add a
+
+
+@[simp]
+def MvPolynomialToSymmetricAlgebra [Module.Free R M] (ι : Type ω) [Fintype ι]
+    (b : Module.Basis ι R M) :
+    MvPolynomial ι R  →ₐ[R]  SymmetricAlgebra R M :=
+  { MvPolynomial.eval₂Hom (algebraMap R (SymmetricAlgebra R M)) (fun (i : ι) =>
+      SymmetricAlgebra.ι R M (b i)) with
+    commutes' := by simp_all
+  }
+
+@[simp]
+def SymmetricAlgebraToMvPolynomial [Module.Free R M] (ι : Type ω) [Fintype ι]
+    (b : Module.Basis ι R M) :
+    SymmetricAlgebra R M →ₐ[R] MvPolynomial ι R :=
+  have φ : M →ₗ[R] MvPolynomial ι R := b.constr R MvPolynomial.X
+
+  { SymmetricAlgebra.lift φ with
+    commutes' := by simp_all
+  }
+
+def SymmectricAlgebraEquivMvPolynomial [Module.Free R M] (σ : Type ω) [Fintype σ]
+    (b : Module.Basis σ R M) :
+    SymmetricAlgebra R M ≃ₐ[R] MvPolynomial σ R where
+  toFun := SymmetricAlgebraToMvPolynomial σ b
+  invFun := MvPolynomialToSymmetricAlgebra σ b
+  left_inv := by
+    intro f
+    induction f using SymmetricAlgebra.induction_basis σ b with
+    | algebraMap => simp
+    | basis i =>
+        simp    
+        suffices h : (SymmetricAlgebra.ι R M) (b i) =
+            (fun₀ | i => (1:R)) i • (SymmetricAlgebra.ι R M) (b i) by
+          rw [ Finset.sum_eq_single i ]
+          · simp
+          · intro j hj hji
+            rw [ Finsupp.single_eq_of_ne ]
+            · simp
+            · aesop
+          · intro hi
+            exfalso
+            exact hi (Finset.mem_univ i)
+        rw [ Finsupp.single_eq_same ]
+        simp
+    | unit => simp
+    | mul f g mf mg =>
+      sorry
+    | add f g mf mg =>
+      sorry
+  right_inv := by
+    intro f
+    induction f using MvPolynomial.induction_on with
+    | C => simp
+    | add => sorry
+    | mul_X => sorry
+  map_mul' := (SymmetricAlgebraToMvPolynomial σ b).map_mul
+  map_add' := (SymmetricAlgebraToMvPolynomial σ b).map_add
+  commutes' := by simp
