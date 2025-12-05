de8f9d95302938b02d1ab887f403d954b12a330e
George McNinch <gmcninch@gmail.com>
2025-12-05 09:52:23 -0500

    asdf

---
+++ LeanAlg/expository/more.lean
@@ -0,0 +1,27 @@
+import Mathlib
+
+#check mul_comm
+#check mul_assoc
+
+example (a b c : ℝ) : a * b * c = b * (a * c) := by
+  rw [mul_comm a b]
+  rw [mul_assoc b a c]
+
+example (a b c : ℝ) : c * b * a = b * (a * c) := by
+  sorry
+
+example (a b c : ℝ) : a * (b * c) = b * (a * c) := by
+  sorry
+
+
+
+variable {α : Type*}
+variable (s t u : Set α)
+open Set
+
+example (h : s ⊆ t) : s ∩ u ⊆ t ∩ u := by
+  rw [subset_def, inter_def, inter_def]
+  rw [subset_def] at h
+  simp only [mem_setOf]
+  rintro x ⟨xs, xu⟩
+  exact ⟨h _ xs, xu⟩
