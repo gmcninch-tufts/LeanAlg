10c5baf5a5f5c992133827278b459277fe93ae11
George McNinch <gmcninch@gmail.com>
2025-12-05 08:53:00 -0500

    Merge branch 'main' of github.com:gmcninch-tufts/LeanAlg

--- LeanAlg/expository/graphs.lean
+++ LeanAlg/expository/graphs.lean
@@ -6,20 +6,46 @@ structure Edge (α : Type) [DecidableEq α] where
   endpoints : Multiset α
   pf : Multiset.card endpoints = 2
 
+
+-- we can construct loops or ordinary edges...
 example : Edge ℕ := Edge.mk {1,1} (by norm_num)
 example : Edge ℕ := Edge.mk {1,2} (by norm_num)
 
--- maybe easier way of constructing an edge
-
-def mkendpoint (a b : α) : Edge α :=
+-- here is a maybe easier way of constructing an edge
+@[simp]
+def mkedge (a b : α) : Edge α :=
   Edge.mk {a,b} (by norm_num)
 
-example : Edge ℕ := mkendpoint 1 1
+def vertex1 : Edge String := mkedge "a" "b"
+def vertex2 : Edge String := mkedge "aa" "aa"
 
-def incident (e : Edge α) (a : α) : Prop :=
+#eval vertex2.endpoints
+#check vertex2.pf
+
+/- `incident a e` is the proposition that
+the edge `e` is incident to the vertex `a`.
+-/
+@[simp]
+def incident (a : α) (e : Edge α) : Prop :=
   a ∈ e.endpoints
 
-structure graph (α : Type) [DecidableEq α] where
-  vertices : Finset α
-  edges : Finset (Edge α)
-  p : ∀ e ∈ edges, (Multiset.toFinset e.endpoints) ⊆ vertices
+noncomputable
+def incidentBool (a : α) (e : Edge α) : Bool :=
+  List.elem a (Multiset.toList e.endpoints)
+
+example : incident "aa" vertex2 := by
+  unfold vertex2
+  simp
+
+structure Graph' (α : Type) [DecidableEq α] where
+  vertices : List α
+  edges : List (Edge α)
+  p : ∀ e ∈ edges, (Multiset.toList e.endpoints) ⊆ vertices
+
+noncomputable
+def incident_edges (G:Graph' α) (v : α) : List (Edge α) :=
+  List.filter (incidentBool v) G.edges
+
+noncomputable
+def degree (G : Graph' α) (v : α) (pv : v ∈ G.vertices) : ℕ :=
+  List.length (incident_edges G v)
